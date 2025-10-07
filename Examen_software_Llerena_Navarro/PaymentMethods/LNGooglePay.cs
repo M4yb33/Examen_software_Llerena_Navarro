a033ec820a1f4b3db149dfc6f36446ebb625ba1a
Gabriel Llerena <gabriel0llerena@gmail.com>
2025-10-07 11:01:10 -0500

    Llenera Navarro: 11H00

---
+++ Examen_software_Llerena_Navarro/PaymentMethods/LNGooglePay.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_software_Llerena_Navarro.PaymentMethods
+{
+    internal class LNGooglePay
+    {
+    }
+}
