a81d0d913c944fa6a3560541293d5a9a708123a0
Gabriel Llerena <gabriel0llerena@gmail.com>
2025-10-07 10:35:05 -0500

    Llerena Navarro: 10H34 Inicio de la prueba

---
+++ Examen_software_Llerena_Navarro/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Examen_software_Llerena_Navarro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
